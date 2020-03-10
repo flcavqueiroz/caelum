007520e4ed719aa0cca391f8a0c08d21e3efb401
flcavqueiroz <flcavqueiroz@gmail.com>
2020-03-09 22:46:06 -0300

    Atualiza conta.py

--- POO/conta.py
+++ POO/conta.py
@@ -1,6 +1,19 @@
 import datetime
+
 class Conta:
+    """
+    Cria uma conta passando quatro atributos:
+        x = Conta(a, b, c, d)
+        a = titular
+        b = numero
+        c = saldo
+        d = limite
+    """
+
     def __init__(self, titular, numero, saldo, limite):
+        """
+        Inicia o objeto Conta e inclui a data em que a conta foi criada
+        """
         print('Criando uma nova conta')
         self.titular = titular
         self.numero = numero
@@ -9,6 +22,10 @@ class Conta:
         self.historico = Historico()
 
     def saca(self, valor):
+        """
+        Função de saque para a conta criada
+            Recebe 2 argumentos: conta e valor a ser sacado
+        """
         if self.saldo < valor:
             return False
         else:
@@ -16,11 +33,20 @@ class Conta:
             self.historico.transacoes.append(f'saque de {valor}')
 
     def deposita(self, valor):
+        """
+        Função de depósito para a conta criada
+            Recebe 2 argumentos: conta e valor a ser depositado
+        """
         self.saldo += valor
         self.historico.transacoes.append(f'depósito de {valor}')
 
 
     def transfere_para(self, destino, valor):
+        """
+        Função que realiza uma transferencia de uma conta origem para outra destino
+            Recebe dois parâmetros: destino e valor
+        """
+
         retira = self.saca(valor)
         if retira == False:
             return False
@@ -31,16 +57,28 @@ class Conta:
             
 
     def extrato(self):
+        """
+        Função que retorna o valor do extrato da conta criada
+            Recebe apenas um parâmetro: conta
+        """
         print(f'numero: {self.numero}\nsaldo: {self.saldo}')
 
 class Historico:
 
     def __init__(self):
-        self.data_abertura = datetime.datetime.today()
+        """
+        Função que determina a data de abertura da conta e suas transações
+        """
+        self.data_abertura = datetime.date.today()
         self.transacoes = []
 
     def imprime(self):
+        """
+        Função que imprime a data de abertura da conta e suas transações
+        """
         print(f'data abertura: {self.data_abertura}')
         print('transações: ')
         for t in self.transacoes:
             print('-', t)
+
+if __name__ == '__main__':
